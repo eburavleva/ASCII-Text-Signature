e750ce9812f62f7d9df6402cec6620046aa420fc
Elena Buravleva <31276462+eburavleva@users.noreply.github.com>
2021-06-07 22:30:07 +0300

    stage 3/4

--- Main.kt
+++ Main.kt
@@ -2,62 +2,112 @@ package signature
 
 fun main() {
     print("Enter name and surname: ")
-    val input = readLine()!!
+    var (name, surname) = readLine()!!.split(" ")
+    name = name.toLowerCase()
+    surname = surname.toLowerCase()
     print("Enter person's status: ")
     val status = readLine()!!
 
-    val a = arrayOf("a", 4, "____", "|__|", "|  |")
-    val b = arrayOf("b", 4, "___ ", "|__]", "|__]")
-    val c = arrayOf("c", 4, "____", "|   ", "|___")
-    val d = arrayOf("d", 4, "___ ", """|  \""", "|__/")
-    val e = arrayOf("e", 4, "____", "|___", "|___")
-    val f = arrayOf("f", 4, "____", "|___", "|   ")
-    val g = arrayOf("g", 4, "____", "| __", "|__]")
-    val h = arrayOf("h", 4, "_  _", "|__|", "|  |")
-    val ii = arrayOf("i", 1, "_", "|", "|")
-    val j = arrayOf("j", 2, " _", " |", "_|")
-    val k = arrayOf("k", 4, "_  _", "|_/ ", """| \_""")
-    val l = arrayOf("l", 4, "_   ", "|   ", "|___")
-    val m = arrayOf("m", 4, "_  _", """|\/|""", "|  |")
-    val n = arrayOf("n", 4, "_  _", """|\ |""", """| \|""")
-    val o = arrayOf("o", 4, "____", "|  |", "|__|")
-    val p = arrayOf("p", 4, "___ ", "|__]", "|   ")
-    val q = arrayOf("q", 4, "____", "|  |", """|_\|""")
-    val r = arrayOf("r", 4, "____", "|__/", """|  \""")
-    val s = arrayOf("s", 4, "____", "[__ ", "___]")
-    val t = arrayOf("t",3, "___", " | ", " | ")
-    val u = arrayOf("u",4, "_  _", "|  |", "|__|")
-    val v = arrayOf("v", 4, "_  _", "|  |", """ \/ """)
-    val w = arrayOf("w",5, "_ _ _", "| | |", "|_|_|")
-    val x = arrayOf("x", 4, "_  _", """ \/ """, """_/\_""")
-    val y = arrayOf("y", 5, "_   _", """ \_/ """, "  |  ")
-    val z = arrayOf("z", 4, "___ ", "  / ", " /__")
-    val space = arrayOf(" ", 4, "    ", "    ", "    ")
+    val letters = arrayOf(
+            arrayOf("____", "___ ", "____",   "___ ",   "____", "____", "____", "_  _", "_", " _",   "_  _"  , "_   ",   "_  _"  ,   "_  _"  , "____", "___ ",   "____"  ,   "____"  , "____", "___", "_  _",   "_  _"  , "_ _ _",   "_  _"  ,   "_   _"  , "___ " ),
+            arrayOf("|__|", "|__]", "|   ", """|  \""", "|___", "|___", "| __", "|__|", "|", " |",   "|_/ "  , "|   ", """|\/|""", """|\ |""", "|  |", "|__]",   "|  |"  ,   "|__/"  , "[__ ", " | ", "|  |",   "|  |"  , "| | |", """ \/ """, """ \_/ """, "  / " ),
+            arrayOf("|  |", "|__]", "|___",   "|__/",   "|___", "|   ", "|__]", "|  |", "|", "_|", """| \_""", "|___",   "|  |"  , """| \|""", "|__|", "|   ", """|_\|""", """|  \""", "___]", " | ", "|__|", """ \/ """, "|_|_|", """_/\_""",   "  |  "  , " /__")
+    )
 
-    val name: Array<String> = input.split("").toTypedArray()
+    val l = countLength(name, surname, status)
+    //println("длина имени $l")
+    var tab1 = 0
+    var tab2 = 0
+    var dif = 0
+    var statusDif = 0
+    var statusTab1 = 0
+    var statusTab2 = 0
+    if (l < status.length) {
+        dif = status.length - l
+        tab1 = dif / 2
+        tab2 = dif - tab1
+    } else {
+        statusDif = l - status.length
+        statusTab1 = statusDif / 2
+        statusTab2 = statusDif - statusTab1
+    }
+    //println("tab1=$tab1, tab2=$tab2")
+    //println("StatusTab1=$statusTab1, tab2=$statusTab2")
 
-    for (j in 1..3) {
-        for (i in name.indices) {
-            print("${name[i][j]}")
-        }
+
+    repeat(l + dif + 6) {
+        print("*")
     }
     println()
-
-
-    for (i in 1..3) {
-        print("${a[i]} ${b[i]} ${c[i]} ${d[i]} ${e[i]} ${f[i]} ${g[i]} ${h[i]} ${ii[i]} ${j[i]} ${k[i]} ${l[i]} ${m[i]} ${n[i]} ${o[i]} ${p[i]} ${q[i]} ${r[i]} ${s[i]} ${t[i]} ${u[i]} ${v[i]} ${w[i]} ${x[i]} ${y[i]} ${z[i]}\n")
+    print("*")
+    repeat(tab1 + 2) {print(" ")}
+    for (l in 0..name.length - 1) {
+        print("${letters[0][name[l].toInt() - 97]} ")
     }
-
-/*
-    repeat(name.length + 4) {
-        print("*")
+    print("     ")
+    for (l in 0..surname.length - 1) {
+        print("${letters[0][surname[l].toInt() - 97]} ")
     }
-    println("\n* $name *")
-    repeat(name.length + 4) {
-        print("*")
+    repeat(tab2 + 1) {print(" ")}
+    print("*\n*")
+    repeat(tab1 + 2) {print(" ")}
+    for (l in 0..name.length - 1) {
+        print("${letters[1][name[l].toInt() - 97]} ")
     }
+    print("     ")
+    for (l in 0..surname.length - 1) {
+        print("${letters[1][surname[l].toInt() - 97]} ")
+    }
+    repeat(tab2 + 1) {print(" ")}
+    print("*\n*")
+    repeat(tab1 + 2) {print(" ")}
+    for (l in 0..name.length - 1) {
+        print("${letters[2][name[l].toInt() - 97]} ")
+    }
+    print("     ")
+    for (l in 0..surname.length - 1) {
+        print("${letters[2][surname[l].toInt() - 97]} ")
+    }
+    repeat(tab2 + 1) {print(" ")}
+    print("*\n*")
 
- */
+    repeat(statusTab1 + 2) {print(" ")}
+    print(status)
+    repeat(statusTab2 + 2) {print(" ")}
+    println("*")
+
+    repeat(l + 6 + dif) { print("*") }
+
+
 }
 
+fun countLength(name: String, surname: String, status: String): Int {
+    val lettersLength = mapOf(
+            'i' to 1,
+            'j' to 2,
+            't' to 3,
+            'w' to 5,
+            'y' to 5
+    )
+    //пройтись по каждой букве имени и фамилии, если она есть в коллекции, то взять ее длину, если нет, то 4
+    var fullNameLength: Int = 0
+    for (i in 0..name.length - 1) {
+        if (lettersLength.containsKey(name[i])) {
+                    fullNameLength = fullNameLength + lettersLength.get(name[i])!! + 1
+                } else {
+        fullNameLength += 5
+        }
+        //print("$i - ${name[i]} - $fullNameLength | ")
+    }
+    for (i in 0..surname.length - 1) {
+        if (lettersLength.containsKey(surname[i])) {
+            fullNameLength = fullNameLength + lettersLength.get(surname[i])!! + 1
+        } else {
+            fullNameLength += 5
+        }
+        //print("$i - ${surname[i]} - $fullNameLength | ")
+    }
+    fullNameLength += 4 // промежуток между именем и фамилией 6
 
+    return fullNameLength
+}
